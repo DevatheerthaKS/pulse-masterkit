ddf29c0f3fe37543474f7d6257dbf97d325c8ed9
DevaTheertha <ksdevatheertha@gmail.com>
2026-06-11 11:05:16 +0530

    Added email automation

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-# main.py
+import os
+import smtplib
 
-print("Hello from Pulse!")
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASS = os.getenv("EMAIL_PASS")
+EMAIL_TO = os.getenv("EMAIL_TO")
+
+subject = "Daily Pulse Report"
+body = "Good Morning! This email was sent automatically by Pulse MasterKit."
+
+message = f"Subject: {subject}\n\n{body}"
+
+with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    server.starttls()
+    server.login(EMAIL_USER, EMAIL_PASS)
+    server.sendmail(
+        EMAIL_USER,
+        EMAIL_TO,
+        message
+    )
+
+print("Email sent successfully!")
